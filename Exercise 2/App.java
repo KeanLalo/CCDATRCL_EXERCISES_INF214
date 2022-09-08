e48d52a7dd7572e583ce7796623e9deb7599ce60
Kean Jerald I. Lalo <111344582+KeanLalo@users.noreply.github.com>
2022-09-09 01:29:48 +0800

    Update App.java

--- Exercise 2/App.java
+++ Exercise 2/App.java
@@ -1,1 +1,35 @@
 
+import java.util.LinkedList;
+
+class Main {
+    public static void main(String[] args) {
+
+        // Creates a new Linkedlist Object
+        LinkedList<String> Playlist = new LinkedList<>();
+
+        // Adds new songs to Playlist
+        Playlist.add("Pare ko");
+        Playlist.add("Overdrive");
+        Playlist.add("Alapaap");
+        Playlist.add("Huling El Bimbo");
+        Playlist.add("Huwag kang Matakot");
+
+        // Adds a new song at the front of the Playlist
+        Playlist.addFirst("With a Smile");
+
+        // Adds a new song at the end of the Playlist
+        Playlist.addLast("Fruitcake");
+
+        // Display node at the front in the LinkedList
+        System.out.println(Playlist.get(0));
+
+        // Replace the last song using the set() method
+        Playlist.set(6, "Magasin");
+
+        // Remove the first song using the remove() method
+        Playlist.remove(0);
+
+        // Display all songs in Playlist
+        System.out.println("Updated playlist: " + Playlist);
+    }
+}
